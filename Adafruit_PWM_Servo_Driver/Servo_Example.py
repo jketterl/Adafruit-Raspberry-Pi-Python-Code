166ecfb9f17f654c754bf1afefc7bf1a645315fc
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-10-25 20:42:03 +0200

    remove obosolete code

--- Adafruit_PWM_Servo_Driver/Servo_Example.py
+++ Adafruit_PWM_Servo_Driver/Servo_Example.py
@@ -11,16 +11,6 @@ import time
 # bmp = PWM(0x40, debug=True)
 pwm = PWM(0x40, debug=True)
 
-def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 60                       # 60 Hz
-  print "%d us per period" % pulseLength
-  pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
-  pulse *= 1000
-  pulse /= pulseLength
-  pwm.setPWM(channel, 0, pulse)
-
 pwm.setPWMFreq(600)                        # Set frequency to 60 Hz
 
 master = 1
