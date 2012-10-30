bfd2f78d880afdbebbebd0791e81eae26de627ec
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-10-30 06:24:30 +0100

    enable websocket control

--- Adafruit_PWM_Servo_Driver/LEDDriver.py
+++ Adafruit_PWM_Servo_Driver/LEDDriver.py
@@ -3,6 +3,10 @@
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 from RGBDevice import RGBDevice
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import json
 
 # Initialise the PWM device using the default address
 pwm = PWM(0x40, debug=True)
@@ -31,7 +35,26 @@ pattern = [
 	{'red':0, 'green':0, 'blue':0}
 ]
 
-while (True): 
-	for value in pattern:
-		device.fadeTo(value)
-		time.sleep(1)
+#while (True): 
+#	for value in pattern:
+#		device.fadeTo(value)
+#		time.sleep(1)
+
+class LEDWebSocket(tornado.websocket.WebSocketHandler):
+	def open(self):
+		pass
+	def on_message(self, message):
+		val = json.loads(str(message))
+		for key in val:
+			val[key] *= 40.95
+		device.setChannels(val)
+	def on_close(self):
+		pass
+
+app = tornado.web.Application([
+	(r"/socket", LEDWebSocket)
+])
+
+if __name__ ==	"__main__":
+	app.listen(8080)
+	tornado.ioloop.IOLoop.instance().start()
